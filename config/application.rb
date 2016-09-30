c264bc47b0770c166625fb3d27780703901485ad
Prokop Simek <prokop.simek@applifting.cz>
2016-09-30 09:58:58 +0200

    add sidekiq as default job processor

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module Gemlifting
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_name_prefix = ENV['ENV_FLAVOR'] || Rails.env
   end
 end
