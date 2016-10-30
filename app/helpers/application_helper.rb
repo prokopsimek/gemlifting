256708c0144198eb5643058c4897375fe90e42af
Prokop Simek <prokop.simek@applifting.cz>
2016-10-30 23:06:38 +0100

    added gem object details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def is_homepage?
     params[:controller] == 'application' && params[:action] == 'home'
   end
+
+  def render_breadcrumbs_for(object)
+    content_tag :ol, class: 'breadcrumb' do
+      breadcrumb = content_tag :li, link_to(fa_icon('home'), root_path)
+      if object.is_a? GemObject
+        breadcrumb << (content_tag :li, link_to(object.gem_category.name, gem_category_path(object.gem_category))) if object.gem_category.present?
+        breadcrumb << (content_tag :li, object.name, class: 'active')
+      end
+
+    end
+
+  end
 end
