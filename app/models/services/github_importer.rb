cc808943f7cfce1440f56933269418c4cde8937c
Prokop Simek <prokop.simek@applifting.cz>
2017-02-21 15:28:22 +0100

    update github_sync_at if import of gem failed

--- app/models/services/github_importer.rb
+++ app/models/services/github_importer.rb
@@ -3,7 +3,7 @@ class Services::GithubImporter
   def import_github_stats(gem_object)
     gh_uri = gem_object.github_uri
 
-    get_info(gh_uri) do
+    get_info(gem_object) do
       repo_hash = Client::Github.new.get_repo_info_hash(gh_uri)
 
       gem_object.update!(
@@ -25,7 +25,7 @@ class Services::GithubImporter
   def import_github_readme(gem_object)
     gh_uri = gem_object.github_uri
 
-    get_info(gh_uri) do
+    get_info(gem_object) do
       readme_content = Client::Github.new.get_repo_readme(gh_uri)
       gem_object.update!(readme: readme_content)
       return self
@@ -34,14 +34,18 @@ class Services::GithubImporter
 
   private
 
-  def get_info(gh_uri)
+  def get_info(gem_object)
+    gh_uri = gem_object.github_uri
+
     if gh_uri.present?
       begin
         yield
       rescue Octokit::InvalidRepository
         Rails.logger.error "Invalid repository: #{gh_uri}"
+        gem_object.update!(github_sync_at: DateTime.now)
       rescue Octokit::NotFound => e
         Rails.logger.error e
+        gem_object.update!(github_sync_at: DateTime.now)
       end
     end
     false
