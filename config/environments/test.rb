c79e92820162da3e69244b7f022cff4016682051
Prokop Simek <prokop.simek@applifting.cz>
2017-05-11 15:45:11 +0200

    set test env host of elasticsearch

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  ENV['ELASTICSEARCH_HOST'] = '127.0.0.1'
 end
